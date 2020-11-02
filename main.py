d56cab51522030301c5597dc5f0e4b5eef9bafe9
Valeria <valeriascherb@gmail.com>
2020-11-02 20:59:32 +0200

    Created main python file

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+Main program
+"""
+
+import json
+
+
+class Main:
+    def run(self):
+        print("I am working!")
+        pass
+
+
+if __name__ == '__main__':
+    m = Main()
+    m.run()
