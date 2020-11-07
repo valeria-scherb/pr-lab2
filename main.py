b29ec643188ca9e6548871b0ce31fd39f737b811
Valeria <valeriascherb@gmail.com>
2020-11-07 13:00:53 +0200

    Implement file reading and check data

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ import json
 
 class Main:
     def run(self):
-        print("I am working!")
+        self.train_data     = self.read_file("train.json")
+        self.classify_data  = self.read_file("classify.json")
+        self.inside         = self.train_data['inside']
+        self.outside        = self.train_data['outside']
+        print('To train:')
+        print('Inside:', self.inside)
+        print('Outside:', self.outside)
+        print('To classify:')
+        print(self.classify_data)
         pass
+
+    def read_file(self, name):
+        with open(name) as f:
+            return json.load(f)
 
 
 if __name__ == '__main__':
