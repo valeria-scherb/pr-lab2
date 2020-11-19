e1e934e8204b5e927b027e9bdf5653addcbfd541
Valeria <valeriascherb@gmail.com>
2020-11-19 23:55:33 +0200

    Add test for actual classifier

--- test_main.py
+++ test_main.py
@@ -5,6 +5,7 @@ Unit tests for the program
 
 import unittest
 import main
+import numpy as np
 
 
 class TestMain(unittest.TestCase):
@@ -29,6 +30,16 @@ class TestMain(unittest.TestCase):
         m = main.Main()
         self.assertEqual(m.ker([2, 3]), [2*2, 2*3, 3*2, 3*3, 2, 3, 1])
         self.assertEqual(m.eigker([2, 3]), [2*2, 2*3, 3*2, 3*3, 0, 0, 0])
+
+    def test_classifier(self):
+        m = main.Main()
+        inside = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        outside = [[3, 0], [0, 3], [-3, 0], [0, -3]]
+        a = np.negative(m.ker(inside[0]))
+        r = m.learn(a, inside, outside)
+        c = m.classify(r, [[0, 0], [0.5, 0.5], [-2, -2], [5, 0], [5, 5], [-0.5, 0]])
+        self.assertEqual(c, [0, 1, 5])
+
 
 
 if __name__ == '__main__':
